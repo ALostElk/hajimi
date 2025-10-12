53d53dcb63dd1a609f156f344d7718d068809a25
ALostElk <wyf@gmail.com>
2025-10-12 13:32:02 +0800

    新增素材

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ from ui import HajimiUI
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("哈基米计算器")
-    root.geometry("400x600")
+    root.geometry("400x800")
     root.resizable(False, False)
     app = HajimiUI(root)
     root.mainloop()
